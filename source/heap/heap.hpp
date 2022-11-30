7f413aea30ad34c1717e42a58b600fc348d4ae31
R2boyo25 <r2boyo25@gmail.com>
2022-11-29 20:09:45 -0600

    Yay, globals and enums and switch cases

--- source/heap/heap.hpp
+++ source/heap/heap.hpp
@@ -6,36 +6,39 @@
 #include"../data/variable.hpp"
 
 class Heap {
-    public:
-        Heap();
+public:
+  Heap();
 
-        bool exists(std::string name);
-        bool isLocal(std::string name);
-        void enterScope(std::string name);
-        void exitScope();
-        std::vector<int> findReferences(std::string name);
-        std::string getScope();
-        
-        Variable get(std::string name);
-        void set(std::string name, Variable value);
-        void printHeap();
+  bool exists(std::string name);
+  bool isLocal(std::string name);
+  void enterScope(std::string name);
+  void exitScope();
+  void setGlobal(std::string name);
+  std::vector<int> findReferences(std::string name);
+  std::string getScope();
+  std::vector<std::string> getGlobals();
+  
+  Variable get(std::string name);
+  void set(std::string name, Variable value);
+  void printHeap();
     
-    private:
-        std::vector<std::string> heapins = {};
-        std::map<std::string, Variable> heapmap = {};
+private:
+  std::vector<std::string> heapins = {};
+  std::map<std::string, Variable> heapmap = {};
 
-        bool alreadyDefinedLocally(std::string name);
+  bool alreadyDefinedLocally(std::string name);
 };
 
-class RegexInstruction {
+class VariableInstruction {
 public:
-  RegexInstruction();
-  RegexInstruction(std::string instruction);
+  VariableInstruction();
+  VariableInstruction(std::string instruction);
   std::string variable_name;
   std::string internal_name;
 };
 
-// var varname internal_variable_name
-// scope scopename
-// TODO: global varname
+// v{ariable} varname internal_variable_name
+// s{cope} scopename
+// e{xitscope}
+// TODO: g{lobal} varname
 //   Tells heap that variable should be kept once scope is left.
